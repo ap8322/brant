0a429175a51d44c700e94ee5b1c55c8bb0f63ce4
羽田 有輝 <yuki.haneda@bizreach.co.jp>
2017-10-21 20:08:28 +0900

    incremental search from tickets cache

---
+++ cmd/create.go
@@ -0,0 +1,62 @@
+// Copyright © 2017 Yuki Haneda
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"os"
+	"strings"
+
+	"github.com/ap8322/brant/config"
+	"github.com/ap8322/brant/ticket"
+	"github.com/spf13/cobra"
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create ticket ID prefix branch",
+	Long:  `create ticket ID prefix branch`,
+	RunE:  create,
+}
+
+func create(cmd *cobra.Command, args []string) error {
+	var tickets ticket.Tickets
+
+	if err := tickets.Load(); err != nil {
+		return err
+	}
+
+	var list string
+
+	for _, t := range tickets.Tickets {
+		list += t.Display + "\n"
+	}
+
+	if err := run(config.Conf.Core.SelectCmd, strings.NewReader(list), os.Stdout); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func init() {
+	RootCmd.AddCommand(createCmd)
+}
